8779e2348de9b31b498a1574899f23a18c09aeb7
dmitrykurmanov <kurmanov.work@gmail.com>
2018-10-21 19:07:05 +0300

    implemented radiogroup for svelte

--- src/svelte/radiogroup.svelte
+++ src/svelte/radiogroup.svelte
@@ -2,27 +2,41 @@
 	{#each question.visibleChoices as item, index}
         <div class={getItemClass(item)} >
             <label class={question.cssClasses.label}>
-                <input type="radio" :name="question.name + '_' + question.id" :value="item.value" :id="question.inputId" v-model="question.value" :disabled="question.isReadOnly" v-bind:aria-label="item.locText.renderedHtml" :class="question.cssClasses.itemControl"/>
-                <span :class="question.cssClasses.materialDecorator"></span>
+                <input type="radio" name={question.name + '_' + question.id} bind:value=item.value id={question.inputId} disabled={question.isReadOnly} aria-label={item.locText.renderedHtml} class={question.cssClasses.itemControl}/>
+                <span class={question.cssClasses.materialDecorator}></span>
                 <span class="check"></span>
-                <span :class="question.cssClasses.controlLabel"><survey-string :locString="item.locText"/></span>
-                <survey-other-choice v-show="question.hasOther && question.isOtherSelected && index === choicesCount" v-if="index == choicesCount" :question="question"/>
+                <span class={question.cssClasses.controlLabel}>
+                  <SurveyString locString={item.locText} />
+                </span>
+                {#if index == choicesCount}           
+                    <OtherChoice question={question} class={applyShowHideClass(question.hasOther && question.isOtherSelected && index === choicesCount)}/>
+                {/if}
             </label>
         </div>
     {/each}
-    <div v-if="question.showClearButton">
-        <input type="button" :class="question.cssClasses.clearButton" v-on:click="function() { question.clearValue(); }" :value="question.clearButtonCaption"/>
-    </div>
-    <legend style="display: none;">{{question.locTitle.renderedHtml}}</legend>
+    {#if question.showClearButton}
+      <div>
+          <input type="button" class={question.cssClasses.clearButton} on:click="doClear()" value={question.clearButtonCaption}/>
+      </div>
+    {/if}
+    <legend style="display: none;">{question.locTitle.renderedHtml}</legend>
 </fieldset>
 
 <script>
+  import SurveyString from "./string.svelte";
+  import OtherChoice from "otherChoice.svelte";
+  import { applyShowHideClass } from "./helpers";
+
   export default {
     data() {
       return {
         question: null,
         css: null
       };
+    },
+    components: {
+      SurveyString,
+      OtherChoice
     },
     methods: {
       getItemClass(item) {
@@ -33,7 +47,13 @@
             : " sv-q-col-" + this.question.colCount);
         if (item.value === this.question.value) itemClass += " checked";
         return itemClass;
+      },
+      doClear() {
+        this.question.clearValue();
       }
+    },
+    helpers: {
+      applyShowHideClass
     },
     computed: {
       choicesCount: ({ question }) => {
