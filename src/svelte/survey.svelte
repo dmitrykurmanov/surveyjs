1317f8eec0eb6a334b25ed328341999f0b07cf1d
dmitrykurmanov <kurmanov.work@gmail.com>
2018-10-24 19:44:38 +0300

    work for survey svelte component

--- src/svelte/survey.svelte
+++ src/svelte/survey.svelte
@@ -16,7 +16,7 @@
 
 				{#if surveyModel.isNavigationButtonsShowing}
 					<div class={css.footer}>
-						<input type="button" bind:value=surveyModel.startSurveyText class={getNavBtnClasses(css, 'start')} on:click="start()" />
+						<input type="button" value={surveyModel.startSurveyText} class={getNavBtnClasses(css, 'start')} on:click="start()" />
 					</div>
 				{/if}
 			</div>
@@ -24,16 +24,21 @@
 
 		{#if surveyModel.state === 'running'}
 			<div class={css.body}>
-				<!-- <survey-progress v-if="survey.isShowProgressBarOnTop" :survey="survey" :css="css"/> -->
-				<!-- <survey-timerpanel v-if="survey.isTimerPanelShowingOnTop" :survey="survey" :css="css"/> -->
+				<!-- <survey-progress v-if="surveyModel.isShowProgressBarOnTop" :survey="survey" :css="css"/> -->
+				<!-- <survey-timerpanel v-if="surveyModel.isTimerPanelShowingOnTop" :survey="survey" :css="css"/> -->
 				<SurveyPage id={surveyModel.currentPage.id} survey={surveyModel} page={surveyModel.currentPage} css={css} />
-				<!-- <survey-timerpanel v-if="survey.isTimerPanelShowingOnBottom" :survey="survey" :css="css"/> -->
-				<!-- <survey-progress style="margin-top: 1em" v-if="survey.isShowProgressBarOnBottom" :survey="survey" :css="css"/> -->
-				<!-- <div v-if="survey.isNavigationButtonsShowing" :class="css.footer">
-						<input type="button" :value="survey.pagePrevText" v-show="!survey.isFirstPage && survey.isShowPrevButton" :class="getNavBtnClasses('prev')" @click="prevPage"/>
-						<input type="button" :value="survey.pageNextText" v-show="!survey.isLastPage" :class="getNavBtnClasses('next')" @click="nextPage"/>
-						<input v-if="survey.isEditMode" type="button" :value="survey.completeText" v-show="survey.isLastPage" :class="getNavBtnClasses('complete')" @click="completeLastPage"/>
-				</div> -->
+				<!-- <survey-timerpanel v-if="surveyModel.isTimerPanelShowingOnBottom" :survey="survey" :css="css"/> -->
+				<!-- <survey-progress style="margin-top: 1em" v-if="surveyModel.isShowProgressBarOnBottom" :survey="survey" :css="css"/> -->
+
+				{#if surveyModel.isNavigationButtonsShowing}
+					<div class={css.footer}>
+							<input type="button" class:sjs-hide="surveyModel.isFirstPage || !surveyModel.isShowPrevButton" value={surveyModel.pagePrevText} class={getNavBtnClasses(css, 'prev')} on:click="prevPage()"/>
+							<input type="button" class:sjs-hide="surveyModel.isLastPage" value={surveyModel.pageNextText} class={getNavBtnClasses(css, 'next')} on:click="nextPage()"/>
+							{#if surveyModel.isEditMode}
+							<input type="button" class:sjs-hide="!surveyModel.isLastPage" value={surveyModel.completeTex} class={getNavBtnClasses(css, 'complete')} on:click="completeLastPage()"/>
+							{/if}
+					</div>
+				{/if}
 			</div>
 		{/if}
 
@@ -49,6 +54,7 @@
 							<span>{surveyModel.completedStateText}</span>
 
 							{#if surveyModel.completedState == 'error'}
+								<input type="button" value={surveyModel.getLocString('saveAgainButton')} on:click="doTrySaveAgain()" class={css.saveData.saveAgainButton} />
 							{/if}
 						</div>
 					</div>
@@ -56,66 +62,91 @@
 			</div>
 		{/if}
 
+		{#if surveyModel.state === 'completedbefore'}
+			<div class={css.body}>
+				{@html surveyModel.processedCompletedBeforeHtml}
+			</div>
+		{/if}
 
+		{#if surveyModel.state === 'loading'}
+			<div class={css.body}>
+				{@html surveyModel.processedLoadingHtml}
+			</div>
+		{/if}
+
+		{#if surveyModel.state === 'empty'}
+			<div class={css.body}>
+				{@html surveyModel.emptySurveyText}
+			</div>
+		{/if}
 	</div>
 </div>
 
 <script>
-	import { surveyCss } from "../defaultCss/cssstandard";
-	import { QuestionRadiogroupModel } from "../question_radiogroup";
-	import SurveyString from "./string.svelte";
-	import SurveyPage from "./page.svelte";
+		import { surveyCss } from "../defaultCss/cssstandard";
+		import { QuestionRadiogroupModel } from "../question_radiogroup";
+		import SurveyString from "./string.svelte";
+		import SurveyPage from "./page.svelte";
 
-	export default {
-	  data() {
-	    return {
-	      surveyModel: null,
-	      css: surveyCss.getCss()
-	    };
-	  },
-	  components: {
-	    SurveyString,
-	    SurveyPage
-	  },
-	  oncreate() {
-	    this.get().surveyModel.onCurrentPageChanged.add((sender, options) => {
-	      this.set({ surveyModel: sender });
-	    });
-	    this.get().surveyModel.onVisibleChanged.add((sender, options) => {
-	      this.set({ surveyModel: sender });
-	    });
-	    this.get().surveyModel.onValueChanged.add((sender, options) => {
-	      this.set({ surveyModel: sender });
-	    });
-	  },
-	  computed: {
-	    hasTitle: ({ surveyModel }) => {
-	      return !!surveyModel.title && surveyModel.showTitle;
-	    },
-	    hasCompletedPage: ({ surveyModel }) => {
-	      return surveyModel.showCompletedPage && surveyModel.state === "completed";
-	    },
-	    completedPageClasses: ({ css }) => {
-	      return css.body + " " + css.completedPage;
-	    },
-	    completedStateClasses: ({ css, surveyModel }) => {
-	      return css.saveData[surveyModel.completedState];
-	    }
-	  },
-	  methods: {
-	    doTrySaveAgain() {
-	      this.get().surveyModel.doComplete();
-	    },
-	    start() {
-	      this.get().surveyModel.start();
-	    }
-	  },
-	  helpers: {
-	    getNavBtnClasses(css, btnType) {
-	      return (css.navigationButton + " " + css.navigation[btnType]).trim();
-	    }
-	  }
-	};
+		export default {
+		  data() {
+		    return {
+		      surveyModel: null,
+		      css: surveyCss.getCss()
+		    };
+		  },
+		  components: {
+		    SurveyString,
+		    SurveyPage
+		  },
+		  oncreate() {
+		    this.get().surveyModel.onCurrentPageChanged.add((sender, options) => {
+		      this.set({ surveyModel: sender });
+		    });
+		    this.get().surveyModel.onVisibleChanged.add((sender, options) => {
+		      this.set({ surveyModel: sender });
+		    });
+		    this.get().surveyModel.onValueChanged.add((sender, options) => {
+		      this.set({ surveyModel: sender });
+		    });
+		  },
+		  computed: {
+		    hasTitle: ({ surveyModel }) => {
+		      return !!surveyModel.title && surveyModel.showTitle;
+		    },
+		    hasCompletedPage: ({ surveyModel }) => {
+		      return surveyModel.showCompletedPage && surveyModel.state === "completed";
+		    },
+		    completedPageClasses: ({ css }) => {
+		      return css.body + " " + css.completedPage;
+		    },
+		    completedStateClasses: ({ css, surveyModel }) => {
+		      return css.saveData[surveyModel.completedState];
+		    }
+		  },
+		  methods: {
+		    doTrySaveAgain() {
+		      this.get().surveyModel.doComplete();
+		    },
+		    start() {
+		      this.get().surveyModel.start();
+		    },
+		    prevPage() {
+		      this.get().surveyModel.prevPage();
+		    },
+		    nextPage() {
+		      this.get().surveyModel.nextPage();
+		    },
+		    completeLastPage() {
+		      this.get().surveyModel.completeLastPage();
+		    }
+		  },
+		  helpers: {
+		    getNavBtnClasses(css, btnType) {
+		      return (css.navigationButton + " " + css.navigation[btnType]).trim();
+		    }
+		  }
+		};
 </script>
 
 <style>
