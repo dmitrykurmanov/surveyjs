b4f9010b435ef2e4db9a5b0c36e6a3b1118934ed
dmitrykurmanov <kurmanov.work@gmail.com>
2018-10-30 18:51:10 +0300

    fixed othed choice for svelte

--- src/svelte/otherChoice.svelte
+++ src/svelte/otherChoice.svelte
@@ -1,6 +1,10 @@
 <div class="form-group">
     {#if !question.isReadOnly}
+      {#if question.getOthersMaxLength() === null}
+        <input type="text" class={question.cssClasses.other || commentClass} bind:value=question.comment aria-label={question.locTitle.renderedHtml}/>
+      {:else}
         <input type="text" class={question.cssClasses.other || commentClass} bind:value=question.comment maxlength={question.getOthersMaxLength()} aria-label={question.locTitle.renderedHtml}/>
+      {/if}
     {:else}
         <div class={question.cssClasses.other}>
             {question.comment}
