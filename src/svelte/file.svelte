dce246ec98202a09b0914b3fb3c7ac2df1d4fd43
dmitrykurmanov <kurmanov.work@gmail.com>
2018-11-15 16:00:17 +0300

    removed commented code

--- src/svelte/file.svelte
+++ src/svelte/file.svelte
@@ -64,15 +64,6 @@
         if (!window["FileReader"] || files.length < 1) return;
         this.refs.input.value = "";
         this.get().question.loadFiles(files);
-        // debugger;
-        // if (!window["FileReader"]) return;
-        // if (!src || !src.files || src.files.length < 1) return;
-        // let files = [];
-        // for (let i = 0; i < src.files.length; i++) {
-        //   files.push(src.files[i]);
-        // }
-        // src.value = "";
-        // this.get().question.loadFiles(files);
       },
       doClean() {
         this.get().question.clear();
