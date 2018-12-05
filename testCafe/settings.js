674ab956885f67e506ece088e581b40bb9771329
dmitrykurmanov <kurmanov.work@gmail.com>
2018-12-05 20:18:07 +0300

    started with f-f tests

--- testCafe/settings.js
+++ testCafe/settings.js
@@ -1,52 +1,57 @@
 import { Selector, ClientFunction } from "testcafe";
 
-export const frameworks = ["knockout", "jquery", "react", "vue"];
+export const frameworks = ["knockout", "jquery", "react", "vue", "svelte"];
 export const url = "http://127.0.0.1:8080/examples/";
 
-export const initSurvey = ClientFunction(
-  (framework, json) => {
+export const initSurvey = ClientFunction((framework, json) => {
+  var model = new Survey.Model(json);
 
+  var surveyComplete = function(model) {
+    window.SurveyResult = model.data;
+  };
+  model.onComplete.add(surveyComplete);
 
-    var model = new Survey.Model(json);
-
-    var surveyComplete = function (model) {
-      window.SurveyResult = model.data;
-    };
-    model.onComplete.add(surveyComplete);
-
-    if (framework === "knockout") {
-      document.getElementById("surveyElement").innerHTML = "";
-      model.render("surveyElement");
-    } else if (framework === "jquery") {
-      document.getElementById("surveyElement").innerHTML = "";
-      $("#surveyElement").Survey({ model: model });
-    } else if (framework === "react") {
-      document.getElementById("surveyElement").innerHTML = "";
-      ReactDOM.render(
-        React.createElement(Survey.Survey, {
-          model: model,
-          onComplete: surveyComplete
-        }),
-        document.getElementById("surveyElement")
-      );
-    } else if (framework === "vue") {
-      document.getElementById("surveyElement").innerHTML =
-        "<survey :survey='survey'/>";
-      !!window.vueApp && vueApp.$destroy();
-      window.vueApp = new Vue({
-        el: "#surveyElement",
-        data: { survey: model }
-      });
-    }
-
-    window.survey = model;
+  if (framework === "knockout") {
+    document.getElementById("surveyElement").innerHTML = "";
+    model.render("surveyElement");
+  } else if (framework === "jquery") {
+    document.getElementById("surveyElement").innerHTML = "";
+    $("#surveyElement").Survey({ model: model });
+  } else if (framework === "react") {
+    document.getElementById("surveyElement").innerHTML = "";
+    ReactDOM.render(
+      React.createElement(Survey.Survey, {
+        model: model,
+        onComplete: surveyComplete
+      }),
+      document.getElementById("surveyElement")
+    );
+  } else if (framework === "vue") {
+    document.getElementById("surveyElement").innerHTML =
+      "<survey :survey='survey'/>";
+    !!window.vueApp && vueApp.$destroy();
+    window.vueApp = new Vue({
+      el: "#surveyElement",
+      data: { survey: model }
+    });
+  } else if (framework === "svelte") {
+    document.getElementById("surveyElement").innerHTML = "";
+    !!window.svelteApp && svelteApp.destroy();
+    window.svelteApp = new Survey.Survey({
+      target: document.querySelector("#surveyElement"),
+      data: {
+        surveyModel: model
+      }
+    });
   }
-);
+
+  window.survey = model;
+});
 
 export const getSurveyResult = ClientFunction(() => window.SurveyResult);
 
 export const setOptions = ClientFunction((questionName, modValue) => {
-  var mergeOptions = function (obj1, obj2) {
+  var mergeOptions = function(obj1, obj2) {
     for (var attrname in obj2) {
       obj1[attrname] = obj2[attrname];
     }
