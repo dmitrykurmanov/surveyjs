c69e4ed19318a2e7236e9594c0197b70db627b1a
dmitrykurmanov <kurmanov.work@gmail.com>
2018-12-09 19:31:46 +0300

    fixed file question

--- src/svelte/file.svelte
+++ src/svelte/file.svelte
@@ -1,16 +1,15 @@
 <div class={question.cssClasses.root}>
     {#if !question.isReadOnly}
-      <input class={question.cssClasses.fileInput} type=file id={question.inputId} 
-          on:change="setValue(this)" bind:files ref:input
-          aria-label={question.locTitle.renderedHtml} 
+      <input class={question.cssClasses.fileInput} type="file" id={question.inputId} 
+          on:change="loadFiles(this)" aria-label={question.locTitle.renderedHtml} 
           {...{ multiple: question.allowMultiple ? 'multiple' : undefined }}
           title={question.inputTitle} accept={question.acceptedTypes} />
+    {/if}
 
-          {#if !question.isReadOnly && !question.isEmpty()}
+    {#if question.isReadOnly && !!question.value}
             <button class={question.cssClasses.removeButton} on:click="doClean()">
               {question.cleanButtonCaption}
             </button>
-          {/if}
     {/if}
 
     {#if question.isReadOnly}
@@ -18,7 +17,7 @@
         placeholder={question.title} style="color: transparent;"/>
     {/if}
 
-    {#if !question.isEmpty()}
+    {#if !!question.previewValue}
       <div>
           {#each question.previewValue as val, index}
             <span key={question.inputId + '_' + index} class:sjs-hide="!val" class={question.cssClasses.preview}>
@@ -54,15 +53,20 @@
     data() {
       return {
         question: null,
-        css: null,
-        files: null
+        css: null
       };
     },
     methods: {
-      setValue(src) {
-        const files = [...this.get().files];
-        if (!window["FileReader"] || files.length < 1) return;
-        this.refs.input.value = "";
+      loadFiles(src) {
+        if (!window["FileReader"]) return;
+        if (!src || !src.files || src.files.length < 1) return;
+
+        let files = [];
+
+        for (let i = 0; i < src.files.length; i++) {
+          files.push(src.files[i]);
+        }
+        src.value = "";
         this.get().question.loadFiles(files);
       },
       doClean() {
